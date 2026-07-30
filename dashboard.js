aa977d8cadcff7e4c4da5c477586a55a710a73e7
skykig <hyperking7733@gmail.com>
2026-07-31 02:00:33 +0530

    Add dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,8 @@
+const chatBtn = document.getElementById("chatBtn");
 
+chatBtn.addEventListener("click", () => {
+    alert("🤖 AI Chat module coming soon!");
+
+    // Agle version me:
+    // window.location.href = "chat.html";
+});
